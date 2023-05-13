94088ddc99ef9be84b200bd622be8651969df4c1
Jul <julianvonmosig@gmail.com>
2023-05-13 14:53:35 +0200

    commit

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -3,11 +3,16 @@ public class GameLogic {
     private boolean isCyclic;
     private int fieldSize;
     private int startPercentage;
+    private Grid grid;
 
     GameLogic(boolean isCyclic, int fieldSize, int startPercentage){
         this.isCyclic = isCyclic;
         this.fieldSize = fieldSize;
         this.startPercentage = startPercentage;
+
+        grid = new Grid(fieldSize);
+        grid.initializeGridRandom(startPercentage);
+        grid.printGrid();
     }
-    
+
 }
